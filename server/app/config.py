34f85a2d4decc36395480976c0eed856141c2d63
allelleo <dev.allelleo@internet.ru>
2024-02-09 00:41:48 +0300

    init commit, подготовка к микросервисам

--- server/app/config.py
+++ server/app/config.py
@@ -1,14 +1,12 @@
-db_url = 'sqlite://database.db'
+db_url = "sqlite://database.db"
 generate_schemas = True
 add_exception_handlers = True
 
 models = [
-    'app.models',
+    "app.models",
 ]
 
-SECRET = 'TulaHackDays2023Secret'
+SECRET = "TulaHackDays2023Secret"
 
-HOST = '127.0.0.1'
+HOST = "127.0.0.1"
 PORT = 8888
-
-queue = []
