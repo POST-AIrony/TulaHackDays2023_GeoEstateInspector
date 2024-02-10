cb42cbe49c88729999dbc77db7d99ce942ec7b8b
allelleo <dev.allelleo@internet.ru>
2024-02-10 11:44:19 +0300

    сдлелал более удобный CLI для запуска

--- service/user/src/cli.py
+++ service/user/src/cli.py
@@ -1,4 +1,22 @@
+import os
+
+import typer
 import uvicorn
+from core import settings
+
+cli = typer.Typer()
+
+
+@cli.command()
+def run():
+    if __name__ == "__main__":
+        uvicorn.run("core.app:app", host=settings.HOST, port=settings.PORT)
+
+
+@cli.command()
+def tdq():
+    os.system("dramatiq core.tdq")
+
 
 if __name__ == "__main__":
-    uvicorn.run("core.app:app")
+    cli()
